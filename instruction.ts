3deec96bc00cc0ce29f7f07f62a027b385f87554
Junwei-Hou <105186599+Junwei-Hou@users.noreply.github.com>
2023-09-07 16:35:38 +0800

    Update instruction.ts

--- instruction.ts
+++ instruction.ts
@@ -7,13 +7,13 @@
 mongod --fork --dbpath data --logpath log/mongo.log --logappend
 
 # Start ali ECS service :
-sudo ssh -i ~/.ssh/kill_liuhuming.pem root@47.109.87.58
+sudo ssh -i ~/.ssh/kill_liuhuming.pem root@your-service-ip
 
 # Upload file to ali ECS service :
-sudo scp -r -i ~/.ssh/kill_liuhuming.pem /Users/junweihou/Desktop/your-file-name root@47.109.87.58:/home
+sudo scp -r -i ~/.ssh/kill_liuhuming.pem /Users/junweihou/Desktop/your-file-name root@your-service-ip:/home
 
 # Start bandwagon service :
-sudo ssh -i ~/.ssh/id_rsa root@67.230.163.195 -p 27287
+sudo ssh -i ~/.ssh/id_rsa root@your-service-ip -p 27287
 
 # Upload file to bandwagon service :
-sudo scp -r -i ~/.ssh/id_rsa -P 27287 /Users/junweihou/Desktop/your-file-name root@67.230.163.195:/data
+sudo scp -r -i ~/.ssh/id_rsa -P 27287 /Users/junweihou/Desktop/your-file-name root@your-service-ip:/data
